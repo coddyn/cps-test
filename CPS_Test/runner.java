e920f6863b34f28f330da4289084ffadd1448632
coddyn <97197585+coddyn@users.noreply.github.com>
2022-06-09 16:33:23 -0400

    Add files via upload

---
+++ CPS_Test/runner.java
@@ -0,0 +1,9 @@
+public class runner{
+    public static void main(String[] args){
+        cpsTestGUI gui = new cpsTestGUI();
+        gui.initializeButtons();
+        gui.setSizes();
+        gui.addToFrame();
+        gui.showWindow();
+    }
+}
