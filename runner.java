db3261df4555d54bf6712181542bc2583d146f42
coddyn <97197585+coddyn@users.noreply.github.com>
2022-06-09 16:33:31 -0400

    Delete runner.java

--- runner.java
+++
@@ -1,9 +0,0 @@
-public class runner{
-    public static void main(String[] args){
-        cpsTestGUI gui = new cpsTestGUI();
-        gui.initializeButtons();
-        gui.setSizes();
-        gui.addToFrame();
-        gui.showWindow();
-    }
-}
